91000909aaa7d0d27dd7f8dcc7b13fb4de392e85
Tobias Lundgren <tobias@lundgrendesign.se>
2017-11-29 01:57:30 +0100

    first commit

---
+++ domain/mamba/registerPlayer.js
@@ -0,0 +1,26 @@
+function RegisterPlayer(name, settings) {
+    const type = 'se.cygni.snake.api.request.RegisterPlayer';
+    const playerName = name || `toblu933`;
+    var settings = settings || {};
+
+    const toString = function () {
+        return `<Type:${type}, playerName:${playerName}, settings:${settings.toString()}>`;
+    };
+
+    const marshall = function () {
+        return {
+            type,
+            playerName,
+            gameSettings: settings.marshall()
+        };
+    };
+
+    return Object.freeze({
+        marshall,
+        toString,
+        type
+    });
+}
+
+exports.new = RegisterPlayer;
+exports.type = RegisterPlayer().type;
---
+++ snakepit/snakestein-bot.js
@@ -0,0 +1,123 @@
+/**
+ * Snake Bot script.
+ */
+const MapUtils = require('../domain/mapUtils.js');
+
+let log = null; // Injected logger
+let prevDirection = '';
+
+
+// FIND POSSIBLE DIRECTIONS
+/**
+ * Get next direction
+ * @param direction
+ * @return direction
+ */
+function findNextDirection(myCoords, map) {
+    // kontrollera vem som är närmst pizzan.
+    let direction = 'UP';
+    let nextDirection;
+    const directionsArray = ['UP', 'DOWN', 'LEFT', 'RIGHT'];
+    const possibleDirections = [];
+
+    // Find possible directions
+    directionsArray.forEach(function (dir) {
+        console.log(`DIR: ${dir}`);
+        console.log(`PREV: ${prevDirection}`);
+        if (MapUtils.canSnakeMoveInDirection(dir, myCoords, map)) {
+                possibleDirections.push(dir);
+                console.log(dir);
+                direction = dir;
+                prevDirection = dir;
+
+        }
+    });
+
+
+
+    // //Find pizzas in direction
+    // for (i = 0; i < possibleDirections.length; i++) {
+    //     if (!MapUtils.getTileInDirection(possibleDirections[i], myCoords, map) === 'food') {
+    //         direction = possibleDirections[i];
+    //
+    //         console.log('food HITTAD!!');
+    //         break;
+    //
+    //     }
+    //     console.log(MapUtils.getTileInDirection(possibleDirections[i], myCoords, map));
+    // No pizzas in possible directions
+    // }
+
+
+    return direction;
+}
+
+function onMapUpdated(mapState, myUserId) {
+    const map = mapState.getMap();
+    let direction = 'DOWN'; // <'UP' | 'DOWN' | 'LEFT' | 'RIGHT'>
+    const snakeBrainDump = {}; // Optional debug information about the snakes current state of mind.
+
+    // 1. Where's what etc.
+    const myCoords = MapUtils.getSnakePosition(myUserId, map);
+
+    log('I am here:', myCoords);
+    snakeBrainDump.myCoords = myCoords;
+
+    // 2. Do some nifty planning...
+    // (Tip: see MapUtils for some off-the-shelf navigation aid.
+
+    direction = findNextDirection(myCoords, map);
+    // console.log(`KÖR HIT: ${direction}`);
+    const tiles = JSON.stringify(MapUtils.getOccupiedMapTiles(map));
+    console.log(`MAPSHIT: ${tiles} \n`);
+
+    // 3. Then shake that snake!
+    return {
+        direction,
+        debugData: snakeBrainDump
+    };
+}
+
+function bootStrap(logger) {
+    log = logger;
+}
+
+function onGameEnded(event) {
+    log('On Game Ended');
+    log(event);
+    // Implement as needed.
+}
+
+function onTournamentEnded(event) {
+    log('On Tournament Ended');
+    log(event);
+    // Implement as needed.
+}
+
+function onSnakeDied(event) {
+    log('On Snake Died');
+    log(event);
+    // Implement as needed.
+}
+
+function onGameStarted(event) {
+    log('On Game Started');
+    log(event);
+    // Implement as needed.
+}
+
+function onGameResult(event) {
+    log('On Game Result');
+    log(event);
+    // Implement as needed.
+}
+
+module.exports = {
+    bootStrap,
+    onGameEnded,
+    onGameResult,
+    onGameStarted,
+    onMapUpdated,
+    onSnakeDied,
+    onTournamentEnded
+};
